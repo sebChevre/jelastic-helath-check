e78f58727a6b601c500a9cd9042214422e84ed72
Sébastien Chèvre <seb.chevre@gmail.com>
2018-10-31 23:36:24 +0100

    initial commit

--- src/main/java/ch/sebooom/tcphealthcheck/application/NoeudsConfig.java
+++ src/main/java/ch/sebooom/tcphealthcheck/application/NoeudsConfig.java
@@ -1,28 +1,41 @@
 package ch.sebooom.tcphealthcheck.application;
 
 import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.HashMap;
 
 @Slf4j
 @Configuration
+@Profile("dev")
 public class NoeudsConfig {
+
+    @Value("${activemq-consumer.host}")
+    private String ACTIVEMQ_CONS_HOST;
+    @Value("${activemq-producer.host}")
+    private String ACTIVEMQ_PROD_HOST;
+    @Value("${activemq-broker.host}")
+    private String ACTIVEMQ_BROKER_HOST;
+
+
 
     @Bean
     public HashMap<String, URI> noeuds(){
         HashMap<String, URI> configuration = new HashMap<>();
-        configuration.put("activemq-consumer",new URI("localhost","/actuator/health",8801));
-        configuration.put("activemq-producer",new URI("localhost","/actuator/health",8802));
-        configuration.put("activemq-broker",new URI("localhost",61616));
+        configuration.put("activemq-consumer",new URI(ACTIVEMQ_CONS_HOST,"/actuator/health",8801));
+        configuration.put("activemq-producer",new URI(ACTIVEMQ_PROD_HOST,"/actuator/health",8802));
+        configuration.put("activemq-broker",new URI(ACTIVEMQ_BROKER_HOST,61616));
         return configuration;
     }
 
 }
 
-
+@ToString
 class URI{
 
     enum PROTOCOLE{
